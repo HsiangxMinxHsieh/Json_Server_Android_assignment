4d5e824cc03eadebc645a94214f8bb0963ddb513
timmymike <t19880916@gmail.com>
2020-08-15 08:42:54 +0800

    1.MainActivity rename to SplashSettingActivity (Layout)

--- app/src/main/java/com/timmymike/json_server_android_assignment/SplashSettingActivity.kt
+++ app/src/main/java/com/timmymike/json_server_android_assignment/SplashSettingActivity.kt
@@ -7,6 +7,6 @@ class SplashSettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_setting)
     }
 }
